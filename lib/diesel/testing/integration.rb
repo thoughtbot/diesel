ee28140850bc8b260d8772410525fb3be92ee33e
Prem Sichanugrist <s@sikachu.com>
2011-06-10 16:55:22 -0400

    Add integration steps for adding gem entry pointing to git repository in Gemfile

--- lib/diesel/testing/integration.rb
+++ lib/diesel/testing/integration.rb
@@ -8,3 +8,6 @@ When /^I add the "([^"]*)" gem from this project$/ do |gem_name|
   append_to_file('Gemfile', %{\ngem "#{gem_name}", :path => "../../.."\n})
 end
 
+When /^I add the "([^"]*)" gem from git "([^"]*)"(?: on branch "([^"]*)")?$/ do |gem_name, git, branch|
+  append_to_file('Gemfile', %{\ngem "#{gem_name}", :git => "#{git}"#{ %{, :branch => "#{branch}"} if branch}})
+end
